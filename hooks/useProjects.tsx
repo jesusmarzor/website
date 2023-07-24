c71b16efdaf4e284b8dc96511c1a84aa789a1628
jesusmarzor <jesusmarzor28@gmail.com>
2023-07-24 21:59:57 +0200

    Add: lang filter in get projects

--- hooks/useProjects.tsx
+++ hooks/useProjects.tsx
@@ -2,22 +2,26 @@ import { LoaderPageConsumer } from "contexts/LoaderPageContext"
 import { useEffect, useState } from "react"
 import { Project } from "utils/interfaces"
 import supabase from "utils/supabase"
+import { useTranslation } from "react-i18next";
 
 const useProjects = () => {
+    const { i18n } = useTranslation()
     const [projects, setProjects] = useState<Project[]>([])
-    const { setIsShowed: setLoaderIsShowed} = LoaderPageConsumer()
+    const { isShowed: loaderIsShowed, setIsShowed: setLoaderIsShowed} = LoaderPageConsumer()
     useEffect(() => {
         supabase
         .from('Projects')
         .select()
+        .eq("lang", i18n.language)
         .then( res => {
+            console.log("Llamada")
             let projects = res.data as Project[]
             projects?.sort((a, b) => b.id - a.id)
             setProjects(projects ?? [])
-            setLoaderIsShowed(false)
+            loaderIsShowed && setLoaderIsShowed(!loaderIsShowed)
         })
 
-    }, [])
+    }, [i18n.language])
     return projects
 }
 
