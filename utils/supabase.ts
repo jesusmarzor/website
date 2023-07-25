5cece33523ca9f9dc088a17d4c2c7245b5efc1c4
jesusmarzor <jesusmarzor28@gmail.com>
2023-07-25 18:14:51 +0200

    Remove: persistSession

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,4 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl: string = "https://" + process.env.NEXT_PUBLIC_SUPABASE_URL ?? ""
 const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
 
-export default createClient(supabaseUrl, supabaseKey)
+export default createClient(supabaseUrl, supabaseKey, {
+    auth: {
+      persistSession: false,
+    }
+})
