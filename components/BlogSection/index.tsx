3142fc7dfc04e32f3956434c90a0ee6a8a2a9e1e
jesusmarzor <jesusmarzor28@gmail.com>
2023-07-29 12:51:00 +0200

    Add: new link to blog

--- components/BlogSection/index.tsx
+++ components/BlogSection/index.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Button from "components/ui/Button"
 import Section from "components/ui/Section"
 import { PostsConsumer } from "contexts/PostsContext"
 import useDate from "hooks/useDatePost"
@@ -6,14 +7,15 @@ import Image from "next/image"
 import Link from "next/link"
 import { useTranslation } from "react-i18next"
 import { maxNumCardsInHomeSections } from "utils/constants"
+import { ButtonType } from "utils/enums"
 
 export const BlogSection: React.FC = () => {
     const { t } = useTranslation()
     const posts = PostsConsumer().slice(0, maxNumCardsInHomeSections)
     const {getDateString, isShowedNew} = useDate()
     return(
-        <Section name="blog" title={t("common.blog")} titleClasses="text-center">
-            <ul className="grid grid-cols-autoFill gap-8 items-center pt-20 pb-5">
+        <Section name="blog" title={t("common.blog")} titleClasses="text-center" sectionClasses="flex flex-col justify-center items-center gap-10">
+            <ul className="grid grid-cols-autoFill gap-8 items-center w-full pt-10">
                 {
                     posts.map( post => {
                         return (
@@ -41,6 +43,7 @@ export const BlogSection: React.FC = () => {
                     })
                 }
             </ul>
+            <Button classes="" type={ButtonType.navigation} to="/blog">Ver todos</Button>
         </Section>
     )
 }
