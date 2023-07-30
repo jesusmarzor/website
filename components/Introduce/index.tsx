f9ad3aecf7a4181410f9cdc2ac18b381390d5cf6
jesusmarzor <jesusmarzor28@gmail.com>
2023-07-30 21:31:49 +0200

    Fix: width introduce

--- components/Introduce/index.tsx
+++ components/Introduce/index.tsx
@@ -10,7 +10,7 @@ import { ButtonType } from "utils/enums"
 export const Introduce: React.FC = () => {
     const { t } = useTranslation()
     return(
-        <Element name="home" className="max-w-7xl mx-auto overflow-hidden pt-20 px-10">
+        <Element name="home" className="max-w-7xl mx-auto overflow-hidden pt-20 px-10 w-full">
             <div className="flex flex-col justify-center items-center gap-4 py-10 minIntroduce:py-20 minIntroduce:flex-row">
                 <section className="text-center minIntroduce:text-start w-full minIntroduce:w-1/2 order-2 minIntroduce:order-1">
                     <h2 className="text-5xl minIntroduce:text-6xl font-bold">{authorName}</h2>
